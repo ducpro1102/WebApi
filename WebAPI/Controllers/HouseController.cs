33236e1f6642c1f5791c1aee12b07b2c72dd3907
Admin <Admin@DESKTOP-B200SEA>
2020-11-25 16:40:49 +0700

    xxx

--- WebAPI/Controllers/HouseController.cs
+++ WebAPI/Controllers/HouseController.cs
@@ -112,14 +112,14 @@ namespace WebAPI.Controllers
 
             ////////////////////////////
             //1.
-            //var claimsIdentity = (ClaimsIdentity)RequestContext.Principal.Identity;
-            //string strClaim = claimsIdentity.Claims.FirstOrDefault(c => c.Type == "sub").Value;
+            var claimsIdentity = (ClaimsIdentity)RequestContext.Principal.Identity;
+            string strUserName = claimsIdentity.Claims.FirstOrDefault(c => c.Type == "UserName").Value;
             ////2.
             //var userIdentity = (ClaimsIdentity)RequestContext.Principal.Identity;
             //var claims = userIdentity.Claims;
             //var roles = claims.Where(c => c.Type == ClaimTypes.Role).ToList();
             ////////////////////////////
-            
+
             GenericService<House> generic = new GenericService<House>();
             var stdList = generic.ExcuteMany("view_all_house", null);
             return Ok(stdList);
