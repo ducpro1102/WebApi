552b5646900b7d8720548ef96c9dda9226cf17d3
ducpro1102 <tduc.tran0211@gmail.com>
2020-11-26 12:19:00 +0700

    duc

--- WebAPI/Controllers/AddressController.cs
+++ WebAPI/Controllers/AddressController.cs
@@ -16,8 +16,8 @@ namespace WebAPI.Controllers
         [HttpGet]
         public IHttpActionResult ViewProvince()
         {
-            var claimsIdentity = (ClaimsIdentity)RequestContext.Principal.Identity;
-            string strUserName = claimsIdentity.Claims.FirstOrDefault(c => c.Type == "UserName").Value;
+            //var claimsIdentity = (ClaimsIdentity)RequestContext.Principal.Identity;
+            //string strUserName = claimsIdentity.Claims.FirstOrDefault(c => c.Type == "UserName").Value;
             GenericService<Province> generic = new GenericService<Province>();
             var proList = generic.ExcuteMany("pro_view_all_province", null);
             return Ok(proList);
