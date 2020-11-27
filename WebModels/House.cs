e69a47f5bf8bc71f0c36d87a143eaf029f597412
Admin <Admin@DESKTOP-B200SEA>
2020-11-27 09:30:32 +0700

    C

--- WebModels/House.cs
+++ WebModels/House.cs
@@ -25,13 +25,12 @@ namespace WebModels
         public long? notoilet { get; set; }
         public string direction { get; set; }
         public string h_description { get; set; }
-        public long price { get; set; }
+        public long? price { get; set; }
         public string b_description { get; set; }
         public string url_Img { get; set; }
         public string url_image1 { get; set; }
         public string url_image2 { get; set; }
         public string url_image3 { get; set; }
         public string user_name { get; set; }
-        public long? price { get; set; }
     }
 }
